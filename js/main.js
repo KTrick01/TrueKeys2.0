357c226c2cbdbc60be28447fe6d4def0bb6068ca
Eduardo Fleitas <fleitaseduardo141@gmail.com>
2022-08-13 17:59:07 -0400

    Sidemenu closes when tap outside

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const closeDrop = document.querySelector("#null");
+const Menu = document.querySelector("#menu")
 
 document.addEventListener('click', function (e){
     if (e.target.closest(".containers")) return
     closeDrop.checked = true;
+
+    if (e.target.closest(".page__header")) return
+    Menu.checked = false;
+    
+        
+    
  
 })
 
